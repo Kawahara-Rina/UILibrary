f26ad2009d5b1bc4224bdd8a3c9c52087ae28a63
KawaharaRina <cyokodaigorou@gmail.com>
2024-10-10 13:58:30 +0900

    ボタンプレファブ生成

---
+++ UILibrary/Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,24 @@
+/*
+    AnimationEvent.cs
+    2024/10/10  Kawahara Rina
+
+    アニメーション中に呼び出す処理や関数をまとめたクラス
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
